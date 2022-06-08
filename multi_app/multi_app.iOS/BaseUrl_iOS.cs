943806ffcec28916f21ad1b782a47403bb5e9099
Artem Brilev <razdvajonez@mail.ru>
2022-06-08 12:30:37 +0300

    init

---
+++ multi_app/multi_app.iOS/BaseUrl_iOS.cs
@@ -0,0 +1,15 @@
+﻿using Foundation;
+using multi_app.iOS;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(BaseUrl_iOS))]
+namespace multi_app.iOS
+{
+    public class BaseUrl_iOS : IBaseUrl
+    {
+        public string Get()
+        {
+            return NSBundle.MainBundle.BundlePath;
+        }
+    }
+}
---
+++ multi_app/multi_app/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace multi_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            hybridWebView.RegisterAction(data => DisplayAlert("Alert", data, "OK"));
+
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            var data = "{ \"firstName\": \"John\", \"lastName\": \"Smith\", \"company\": \"Google\" }";
+            hybridWebView.EvaluateJavaScriptAsync($"updatetextonwebview('{data}')");
+            Console.WriteLine("C# button clicked");
+        }
+    }
+}
+public interface IBaseUrl { string Get(); }
